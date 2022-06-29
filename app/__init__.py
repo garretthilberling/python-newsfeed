9e46f320da68cfe4e42c4ac782da96243ef8f02b
garretthilberling <ghilberling@knights.ucf.edu>
2022-06-28 20:01:15 -0400

    trying wsgi.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,25 @@ from app.routes import home, dashboard, api
 from app.db import init_db
 from app.utils import filters
 
+
 def create_app(test_config=None):
     # set up app config
     app = Flask(__name__, static_url_path='/')
     app.url_map.strict_slashes = False
     app.config.from_mapping(
         SECRET_KEY='super_secret_key'
-    )   
+    )
     app.jinja_env.filters['format_url'] = filters.format_url
     app.jinja_env.filters['format_date'] = filters.format_date
     app.jinja_env.filters['format_plural'] = filters.format_plural
 
-    app.run(debug=True)
+    app.config.update(
+        DEBUG=True,
+        PROPAGATE_EXCEPTIONS=True
+    )
+
+    if __name__ == "__main__":
+        app.run(threaded=True, host='0.0.0.0')
 
     # register routes
     app.register_blueprint(home)
